cde001007b2cafbb309b6f41c8c11ec5b1a933d7
Hoang <hoang23101995@gmail.com>
2017-05-24 16:00:13 +0700

    add login page

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,46 @@
+class SessionsController < ApplicationController
+  before_action :not_login_user, only: [:new]
+
+  def new
+    @user = User.new
+    render layout: "users_form"
+  end
+
+  def create
+    if @user = login(params[:session][:email], params[:session][:password], params[:session][:remember])
+      flash[:success] = "Logged in successfully"
+      respond_to do |format|
+        format.html { redirect_back_or_to root_path}
+        format.js
+      end
+    else
+      user = User.find_by_email(params[:session][:email])
+      if user && user.activation_state == "pending"
+        flash[:warning] = "User is inactivation"
+        @inactivation = true
+      else
+        flash[:error] = "Invalid Email or Password"
+      end
+      respond_to do |format|
+        format.html { render action: 'new'}
+        format.js
+      end
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_path
+    flash[:success] = "Logout successfully"
+  end
+
+  private
+
+  def not_login_user
+    if logged_in?
+      redirect_to root_path
+      flash[:warning] = "Logged in successfully"
+    end
+  end
+
+end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,24 +1,18 @@
 class UserMailer < ActionMailer::Base
   default from: "2310khonggilakhongthe28@gmail.com"
 
-  def reset_password_email(user)
-    @user = user
-    @url  = edit_password_reset_url(user.reset_password_token)
-    mail(:to => user.email,
-         :subject => "#{t('mailer.subject.reset_password')}")
-  end
 
   def activation_needed_email(user)
     @user = user
-    @url  = "http://127.0.0.1:3000/users/#{user.activation_token}/activate"
+    @url  = "http://127.0.0.1:3000/actives/#{user.activation_token}/update"
     mail(:to => user.email,
-         :subject => "#{t('mailer.subject.welcome')}")
+         :subject => "Welcome to the Music Share")
   end
 
   def activation_success_email(user)
     @user = user
     @url  = "http://127.0.0.1:3000/login"
     mail(:to => user.email,
-         :subject => "#{t('mailer.subject.done')}")
+         :subject => "Your account is confirmed")
   end
 end
